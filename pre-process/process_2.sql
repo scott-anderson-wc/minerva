93ef5eeacf8b27e12d1298c72e705c2050f715e8
Scott D. Anderson <scott.anderson@wellesley.edu>
2017-07-24 11:31:21 -0700

    type cast during creation of insulin_carbs_2

--- pre-process/process_2.sql
+++ pre-process/process_2.sql
@@ -13,11 +13,31 @@ use janice;
 
 # the following makes it idempotent
 
-delete from insulin_carb_2;
+drop table if exists insulin_carb_2;
+CREATE TABLE `insulin_carb_2` (
+ `user` varchar(20) NOT NULL,
+ `date_time` datetime NOT NULL,
+ `basal_amt` decimal(10,3), 	-- note, lower case
+ `temp_basal_down` varchar(5),
+ `temp_basal_up` varchar(5),
+ `temp_basal_duration` varchar(10),
+ `bolus_type` varchar(15) NOT NULL,
+ `bolus_volume` decimal(10,2),
+ `Immediate_percent` varchar(8),
+ `extended_percent` varchar(9),
+ `duration` varchar(9),
+ `carbs` decimal(10,2),
+ `notes` text NOT NULL,
+ `rec_num` int(9) NOT NULL
+AUTO_INCREMENT,
+PRIMARY KEY (`rec_num`),
+KEY `rec_num` (`rec_num`)
+);
 
 insert into insulin_carb_2(
        user,date_time,
-       Basal_amt,temp_basal_down,temp_basal_up,temp_basal_duration,
+       basal_amt,
+       temp_basal_down,temp_basal_up,temp_basal_duration,
        bolus_type,bolus_volume,
        Immediate_percent, extended_percent,
        duration,
@@ -26,11 +46,13 @@ insert into insulin_carb_2(
        rec_num)
 select 
        user,str_to_date(date_time,'%Y%m%d%H%i'),
-       Basal_amt,temp_basal_down,temp_basal_up,temp_basal_duration,
-       bolus_type,bolus_volume,
+       if(Basal_amt='',NULL,cast(Basal_amt as decimal(10,3))),
+       temp_basal_down,temp_basal_up,temp_basal_duration,
+       bolus_type,
+       if(bolus_volume='',NULL,cast(bolus_volume as decimal(10,2))),
        Immediate_percent, extended_percent,
        duration,
-       carbs,
+       if(carbs='',NULL,cast(carbs as decimal(10,2))),
        notes,
        rec_num
  from insulin_carb_1;
