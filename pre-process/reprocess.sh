45331f0339db815f046dc347dbd6db2173711066
Scott D. Anderson <scott.anderson@wellesley.edu>
2018-10-30 12:15:29 -0400

    fixed logging and tee statements

--- pre-process/reprocess.sh
+++ pre-process/reprocess.sh
@@ -13,23 +13,23 @@ fi
 touch $log
 
 echo "logfile is $log"
-echo "delete bogus insulin_carb and cgm data, making _1 tables, and coercing datatypes" | tee >> $log
-mysql < process_1.sql >> $log
-echo "make rtime values and basal_gap fields in insulin_carb and cgm, making _2 tables" | tee >> $log
-mysql < process_2.sql >> $log
-echo "identify basal gaps (no change for 24 hours)"
-./no-basal-changes.py update
-echo "basal changes fix"
-mysql -e "UPDATE insulin_carb_2 SET basal_gap = 0 WHERE rec_num = 87991;"
-echo "fix the funky off-by-one-day error" | tee >> $log
-mysql -e "UPDATE insulin_carb_2 SET date_time = date_add(date_time,interval 1 day) WHERE '2016-04-01 21:25' <= date_time and date_time <= '2016-05-02 18:21';"
-echo "groups and deal with duplicates" | tee >> $log
-mysql < dup-cgm-readings-rtimes.sql >> $log
-mysql < dup-mgm-readings-rtimes.sql >> $log
-echo "identify duplicates in insulin_carb." | tee >> $log
-mysql < dup-insulin_carb-entries.sql >> $log
-echo "make ICS" | tee >> $log
-mysql < ../sql/make-insulin_carb_smoothed.sql >> $log
+echo "delete bogus insulin_carb and cgm data, making _1 tables, and coercing datatypes" | tee -a $log
+mysql < process_1.sql | tee -a $log
+echo "make rtime values and basal_gap fields in insulin_carb and cgm, making _2 tables" | tee -a $log
+mysql < process_2.sql | tee -a $log
+echo "identify basal gaps (no change for 24 hours)" | tee -a $log
+./no-basal-changes.py update | tee -a $log
+echo "basal changes fix" | tee -a $log
+mysql -e "UPDATE insulin_carb_2 SET basal_gap = 0 WHERE rec_num = 87991;" | tee -a $log
+echo "fix the funky off-by-one-day error" | tee -a $log
+mysql -e "UPDATE insulin_carb_2 SET date_time = date_add(date_time,interval 1 day) WHERE '2016-04-01 21:25' <= date_time and date_time <= '2016-05-02 18:21';" | tee -a $log
+echo "groups and deal with duplicates" | tee -a $log
+mysql < dup-cgm-readings-rtimes.sql | tee -a $log
+mysql < dup-mgm-readings-rtimes.sql | tee -a $log
+echo "identify duplicates in insulin_carb." | tee -a $log
+mysql < dup-insulin_carb-entries.sql | tee -a $log
+echo "make ICS" | tee -a $log
+mysql < ../sql/make-insulin_carb_smoothed.sql | tee -a $log
 # insulin_carb_smoothed is now made, but empty. It's filled using dynamic_insulin:
-python ../dynamic_insulin.py 
-./export-ICS2-to-TSV.sh
+python ../dynamic_insulin.py | tee -a $log
+./export-ICS2-to-TSV.sh | tee -a $log
