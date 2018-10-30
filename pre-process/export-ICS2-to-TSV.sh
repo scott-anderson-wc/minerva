205615ad8adada17c3c0ff7c19a868ba80d55540
Scott D. Anderson <scott.anderson@wellesley.edu>
2018-10-30 12:15:06 -0400

    output file should be named ICS2 rather than ICS

--- pre-process/export-ICS2-to-TSV.sh
+++ pre-process/export-ICS2-to-TSV.sh
@@ -2,10 +2,10 @@
 
 dir="$HOME/scott/devel/exports"
 
-filename=`date +${dir}/ics-%F.csv`
+filename=`date +${dir}/ics2-%F.csv`
 
 if [ -e $filename ]; then
-    filename=`date +${dir}/ics-%F-%R.csv`
+    filename=`date +${dir}/ics2-%F-%R.csv`
 fi
 
 mysql -e 'select * from insulin_carb_smoothed_2;' > $filename
