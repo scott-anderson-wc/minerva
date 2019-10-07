dbfbb6234ccb7ff4e9b907ca929093d5cd84d1d1
Scott D. Anderson <scott.anderson@wellesley.edu>
2019-10-07 10:47:06 -0400

    many changes to isf2.py

--- isf2.py
+++ isf2.py
@@ -20,13 +20,14 @@ Implemented by Scott 1/22/2019
 
 '''
 
-
+import sys
 import MySQLdb
 import dbconn2
 import csv
 import math
 import itertools
 from datetime import datetime, timedelta,date
+import dateparser
 import decimal                  # some MySQL types are returned as type decimal
 from dbi import get_dsn, get_conn # connect to the database
 import date_ui
@@ -278,6 +279,7 @@ where rtime>= %s and rtime<= addtime(%s,'2:00')''',
     return curs.fetchall()
 
 def get_isf_at(conn,rtime):
+    '''Return the first actual ISF value at or after the given time'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     # in case rtime is not exact, find the first value that is >= to the given string
     curs.execute('''SELECT rtime from insulin_carb_smoothed_2 
@@ -393,10 +395,471 @@ def getRecentISF (time_bucket, num_weeks, min_data, debug=False):
 
     return doubling_up(num_weeks)
 
-# hi there!
+# ================================================================
+# get an ISF estimate for a point in time based on data prior to that time.
 
-# new code to recompute ISF values from command line
+
+# from https://stackoverflow.com/questions/24101524/finding-median-of-list-in-python
+
+def median(lst):
+    '''return median of list; list is modified'''
+    quotient, remainder = divmod(len(lst), 2)
+    lst.sort()
+    if remainder:
+        return lst[quotient]
+    return sum(lst[quotient - 1:quotient + 1]) / 2.
+
+def to_list(tuple_seq):
+    '''MySQL gives us a tuple of tuples, but we need a list of numbers'''
+    return [ x[0] for x in tuple_seq ]
+
+min_rtime_cache = None
+
+def min_rtime():
+    global min_rtime_cache
+    if min_rtime_cache is None:
+        conn = get_conn()
+        curs = conn.cursor()
+        curs.execute('''select min(rtime) from insulin_carb_smoothed''')
+        min_rtime_cache = curs.fetchone()[0]
+    return min_rtime_cache
+    
+def max_rtime():
+    conn = get_conn()
+    curs = conn.cursor()
+    curs.execute('''select max(rtime) from isf_details''')
+    return curs.fetchone()[0]
+
+def datetime_bucket(dt):
+    '''returns the bucket: 0-22'''
+    return dt.hour//2*2
+
+def datetime_quarter(dt):
+    '''returns the quarter: 1-4'''
+    return 1+(dt.month-1)//3
+
+def datetime2quarter_start(dt):
+    '''Returns the first date in the quarter, and the first hour in the time bucket.'''
+    return datetime(dt.year,
+                    (dt.month-1)//3*3+1,
+                    1,
+                    datetime_bucket(dt),
+                    0)
+
+def test_datetime2quarter_start():
+    aug1=datetime(2018,8,1,11,0)
+    for delta_day in range(15):
+        for delta_time in range(40):
+            x = aug1+timedelta(days=delta_day,minutes=delta_time*10)
+            y = datetime2quarter_start(x)
+            print('{} => {}'.format(x,y))
+
+def delta_quarter(dt,dq):
+    '''Returns a datetime in the preceding or following quarters,
+where dq is the delta in quarters. Assumes 12 weeks earlier will 
+get the prior quarters, and 14 weeks later will get the following ones.'''
+    delta_weeks = dq * 12 if dq < 0 else dq * 14
+    alt = dt + timedelta(weeks=delta_weeks)
+    return datetime2quarter_start(alt)
+
+def test_delta_quarter():
+    dt = dateparser.parse('7/1/2016 14:00')
+    for dq in range(-4,3):
+        print(dt,dq,delta_quarter(dt,dq))
+
+# ================================================================
+
+# cache for real ISF values, indexed by (bucket, year, quarter)
+isf_cache = {'hits':0,'misses':0}
+
+ignore_isf_cache = False
+
+def isf_values(curs, bucket, rtime):
+    '''This cached lookup gets the ISF values for the quarter that the
+rtime is in, which might include future values.
+    '''
+    year = rtime.year
+    quarter = datetime_quarter(rtime)
+    key = (bucket, year, quarter)
+    if (ignore_isf_cache or key not in isf_cache):
+        isf_cache['misses'] += 1
+        curs.execute('''select isf from isf_details 
+                        where time_bucket(rtime) = %s
+                        and year(rtime)=%s and quarter(rtime)=%s''',
+                     key)
+        vals = [ tup[0] for tup in curs.fetchall() ]
+        isf_cache[key] = vals
+    else:
+        isf_cache['hits'] += 1
+    return isf_cache[key]
+
+def test_isf_values(dt=dateparser.parse('3/14/2015 1:15am')):
+    '''gets ISF values twice; the second should be cached. The first rtime
+is the argument and the second is the next quarter (92 days later).
+    '''
+    conn = get_conn()
+    curs = conn.cursor()
+    dt0 = min_rtime()
+    print(isf_values(curs, 18, dt0))
+    print(isf_values(curs, 18, dt0))
+    print(isf_values(curs, 18, dt0 + timedelta(days=3*92)))
+    print(isf_values(curs, 18, dt0 + timedelta(days=3*92)))
+    print(isf_cache)
+
+# cache for estimated ISF values, indexed by (bucket, year, quarter)
+isf_est_cache = {}
+
+def isf_at_time(dt,
+                min_data=20, # 25 doesn't work for dt=3/15/2014 06:00
+                debug=False):
+    '''Return an estimated ISF value to use for the given datetime (dt), 
+and seeking at least min_data. Any date in the same quarter will yield the same value.
+We may use values from neighboring quarters and buckets.'''
+    dt = dt if type(dt) == datetime else dateparser.parse(dt)
+    year, quarter = dt.year, datetime_quarter(dt)
+    bucket = datetime_bucket(dt)
+    key = (bucket, year, quarter)
+    if debug and key in isf_est_cache:
+        del isf_est_cache[key]
+    if key not in isf_est_cache:
+        # date at the beginning of the quarter
+        dtq_curr = datetime2quarter_start(dt)
+        conn = get_conn ()
+        curs = conn.cursor()
+        prev_bucket = (bucket - 2) % 24
+        next_bucket = (bucket + 2) % 24
+        for q0,q1,neighbors,option in [(0,0,False,'A'),
+                                       (0,0,True,'A2'),
+                                       (-1,0,False,'B'),
+                                       (-1,0,True,'B2'),
+                                       (-2,0,False,'C'),
+                                       (-2,0,True,'C2'),
+                                       # look forward
+                                       (-2,1,False,'D'),
+                                       (-2,1,True,'D2')]:
+            if debug:
+                print('starting',q0,q1,neighbors,option)
+            vals = []
+            for q in range(q0,q1+1):
+                dtq_other = delta_quarter(dtq_curr,q)
+                vals += isf_values(curs,bucket,dtq_other)
+                if debug:
+                    print('got {} values'.format(len(vals)))
+                if neighbors:
+                    vals += isf_values(curs,prev_bucket,dtq_other)
+                    if debug:
+                        print('with prev neighbor buckets {} got {} values'.format(prev_bucket,len(vals)))
+                    vals += isf_values(curs,next_bucket,dtq_other)
+                    if debug:
+                        print('with next neighbor buckets {} got {} values'.format(next_bucket,len(vals)))
+            # is it enough?
+            if len(vals) >= min_data:
+                isf_est_cache[key] = round(median(vals),2), option, len(vals)
+                if debug:
+                    print('done option {} with {} values'.format(option,len(vals)))
+                break
+        if key not in isf_est_cache:
+            # if we get to here, every option failed. Just return the last
+            isf_est_cache[key] = round(median(vals),2), 'FAIL', len(vals)
+    # after the skippable computation, just use cached value
+    return isf_est_cache[key]
+
+class stats:
+    def __init__(self):
+        # let's start with the trivial implementation that keeps all values
+        self.vals = []
+        self.count = 0
+        self.sum = 0
+        self.sum_squares = 0
+        self.min = None
+        self.max = None
+
+    def update(self,val):
+        self.vals.append(val)
+        self.count += 1
+        self.sum += val
+        self.sum_squares += val*val
+        if self.min is None or val < self.min:
+            self.min = val
+        if self.max is None or val > self.max:
+            self.max = val
+
+    def report(self):
+        # see https://www.thoughtco.com/sum-of-squares-formula-shortcut-3126266
+        if self.min == self.max:
+            # if all values are the same, numerical roundoff in shortcut can yield negative SSD
+            std_dev = 0.0
+        else:
+            sum_squared_deviations = self.sum_squares - self.sum*self.sum/self.count
+            std_dev = math.sqrt(sum_squared_deviations/self.count)
+        return {'count': self.count,
+                'mean': self.sum/self.count,
+                'min': self.min,
+                'max': self.max,
+                'stddev': std_dev,
+                'median': median(self.vals)}
+
+
+def test_stats():
+    s = stats()
+    for x in range(10):
+        s.update(x)
+    print(s.report())
+
+
+def test_isf_at_time(min_dt=min_rtime(),max_dt=max_rtime()):
+    all_count = 0
+    counts = {}
+    isfs = []
+    conn = get_conn ()
+    curs = conn.cursor()
+    print('iterating from {} to {}'.format(min_dt,max_dt))
+    dt = min_dt
+    curr_quarter = datetime_quarter(dt)
+    print('starting at',datetime.now().strftime('%H:%M:%S'))
+    st = [ stats() for b in range(0,24) ]
+    while dt < max_dt:
+        if datetime_quarter(dt) != curr_quarter:
+            curr_quarter = datetime_quarter(dt)
+            print('at',datetime.now().strftime('%H:%M:%S'))
+            print('Now in {}/{}'.format(dt.year,curr_quarter))
+            print('option counts',counts)
+            print('isf cache',len(isf_cache),isf_cache['hits'],isf_cache['misses'])
+            
+            for bucket in range(0,24,2):
+                if st[bucket].min < st[bucket].max:
+                    print('stats for {}'.format(bucket),st[bucket].report())
+            global gst
+            gst = st
+            # reset the stats
+            st = [ stats() for b in range(0,24) ]
+        # normal processing
+        isf,opt,n = isf_at_time(dt, debug=False)
+        isfs.append(isf)
+        (st[ 2*math.floor(dt.hour/2.0) ]).update(isf)
+        # print((dt,isf,opt))
+        counts[opt] = 1 + counts.get(opt,0)
+        all_count += 1
+        dt = dt + timedelta(minutes=5)
+    print('Estimated ISF {} times'.format(all_count))
+    print('results were like this: ')
+    print(counts)
+    global gisfs
+    gisfs = isfs
+    print(min(isfs), max(isfs))
+
+# ================================================================
+# some of the interpolated ISF values computed by the function above
+# are weird:  54.51084999999935 and many others with many decimal
+# places. That's odd. I'm going to find out how long they are:
+
+def long_isf_values():
+    longest = 1
+    conn = get_conn()
+    curs = conn.cursor()
+    curs.execute('''select isf from isf_details''')
+    for row in curs.fetchall():
+        isf = row[0]
+        if len(str(isf)) > longest:
+            longest = len(str(isf))
+            print('next longest:',isf)
+
+
+# ================================================================
+
+def isf_density():
+    conn = get_conn()
+    curs = conn.cursor()
+    # this could be way more efficient
+    for year in range(2014,2019):
+        # for month in range(1,13):
+        for quarter in range(1,5):
+            # vals = [str(year),str(month)]
+            vals = [str(year),str(quarter)]
+            for bucket in range(0,24,2):
+                dt = datetime(year,(quarter-1)*3+1,1,bucket,0)
+                curs.execute('''select count(*) from isf_details
+                                where year(rtime)=%s and quarter(rtime)=%s
+                                and time_bucket(rtime)=%s''',
+                             [year,quarter,bucket])
+                count = curs.fetchone()[0]
+                vals.append(str(count))
+            print('\t'.join(vals))
+
+def est_isf_values():
+    conn = get_conn()
+    curs = conn.cursor()
+    headers = ['YYYY','Q']
+    headers.extend([str(b) for b in range(0,24,2)])
+    print('\t'.join(headers))
+    # this could be way more efficient
+    for year in range(2014,2019):
+        # for month in range(1,13):
+        for quarter in range(1,5):
+            # vals = [str(year),str(month)]
+            vals = [str(year),str(quarter)]
+            for bucket in range(0,24,2):
+                dt = datetime(year,(quarter-1)*3+1,1,bucket,0)
+                isf,opt,n = isf_at_time(dt)
+                vals.append(str(n))
+            print('\t'.join(vals))
+
+# ================================================================
+
+def tsv_out(*args):
+    print('\t'.join(map(str,args)))
+
+def est_isf_table():
+    '''Write out the EST_ISF values in TSV format 
+for reading into a MySQL table to pre-compute them
+Columns are year,quarter,bucket,isf,option,n
+where option are how the data was computed and 
+n is the number of real values that the median is
+computed from.'''
+    conn = get_conn()
+    curs = conn.cursor()
+    for year in range(2014,2019):
+        # for month in range(1,13):
+        for quarter in range(1,5):
+            # vals = [str(year),str(month)]
+            vals = [str(year),str(quarter)]
+            for bucket in range(0,24,2):
+                dt = datetime(year,(quarter-1)*3+1,1,bucket,0)
+                isf,opt,n = isf_at_time(dt)
+                tsv_out(year,quarter,bucket,isf,opt,n)
+                
+# ================================================================
+
+def regression_data():
+    '''Regression data will start with Q2 of 2014 and go to Q4 of 2018, 
+the last quarter for which we have good data'''
+    conn = get_conn()
+    curs = conn.cursor()
+    est_isf = {}
+    curs.execute('select year,quarter,time_bucket,isf_est from isf_est')
+    for row in curs.fetchall():
+        (y,q,t,i) = row
+        if '2014-1' < y+'-'+q <= '2018-4':
+            est_isf[(y,q,t)] = i
+    # print('isf_est.keys()')
+    for row in sorted(est_isf.keys()):
+        # print(row)
+        pass
+    # print('iterate over insulin_carb_smoothed_2')
+    # we need 3 good rows in a row: the difference between
+    # the first two gives us the bg_slope, and we try to
+    # predict the third. 
+    prev_prev_bg = None
+    prev_bg = None
+    prev_di = None
+    curs.execute('''
+select rtime,cgm,bg,dynamic_insulin 
+from insulin_carb_smoothed_2
+where rtime >= '2014-04-01' 
+  and rtime < '2019-01-01';
+''')
+    tsv_out('good_bg','prev_bg','bg_slope','isf','di','di_slope')
+    linear, nonlinear = 0,0
+    for row in curs.fetchall():
+        (rtime,cgm,bg,di) = row
+        if di is None or (cgm is None and bg is None):
+            prev_prev_bg = None
+            continue
+        good_bg = bg if bg is not None else cgm
+        if prev_prev_bg is None:
+            prev_prev_bg = good_bg
+            continue
+        index = str(rtime.year), str((rtime.month-1)//3+1), str((rtime.hour//2)*2)
+        isf = est_isf[index]    # could avoid this lookup, but not going to bother
+        if prev_bg is None:
+            prev_bg,prev_di = (good_bg,di)
+            continue
+        bg_slope = prev_bg - prev_prev_bg
+        di_slope = prev_di - di
+        # sanity test
+        if bg == (prev_bg + bg_slope):
+            linear += 1
+        else:
+            nonlinear += 1
+        tsv_out(good_bg,prev_bg,bg_slope,isf,di,di_slope)
+        # shift current values to prev values
+        prev_prev_bg,prev_bg,prev_di = (prev_bg,good_bg, di)
+    # afterwards, print how many linear and nonlinear steps we had
+    print('linear: {} nonlinear: {}'.format(linear,nonlinear),file=sys.stderr)
+
+# ================================================================
+# The regression table computed by the previous code, pulling out
+# just 20K rows near the end, gave us the following regression
+# model (dropping the ISF value, which was not significant)
+# and rounding numbers to a reasonable number of digits (3).
+
+# predicted_bg = 2.7 + 0.985 * prev_bg - 0.228 * bg_slope - 0.175 * di - 1.3 * di_slope
+
+MODEL = {'constant': 2.7, 'prev_bg': 0.985, 'bg_slope': 0.288, 'di': 0.175, 'di_slope': 1.3}
+
+# the following code implements that model, filling in those values into the
+# Predicted_BG column that Anah added to insulin_carb_smoothed_2.
+
+def compute_predicted_bg():
+    '''implements the regression model and updates the insulin_carb_smoothed_2 table 
+with the predictions. These are just one-step predictions, not 2 hour extrapolations.'''
+    conn = get_conn()
+    curs = conn.cursor()
+    update = conn.cursor()
+    # we need 3 good rows in a row: the difference between
+    # the first two gives us the bg_slope, and we predict 
+    # the third. 
+    prev_prev_bg = None
+    prev_bg = None
+    prev_di = None
+    curs.execute('''
+select rtime,cgm,bg,dynamic_insulin 
+from insulin_carb_smoothed_2
+where rtime >= '2014-04-01' 
+  and rtime < '2019-01-01';
+''')
+    for row in curs.fetchall():
+        (rtime,cgm,bg,di) = row
+        if di is None or (cgm is None and bg is None):
+            prev_prev_bg = None
+            continue
+        good_bg = bg if bg is not None else cgm
+        if prev_prev_bg is None:
+            prev_prev_bg = good_bg
+            continue
+        if prev_bg is None:
+            prev_bg,prev_di = (good_bg,di)
+            continue
+        bg_slope = prev_bg - prev_prev_bg
+        di_slope = prev_di - di
+        # compute regression model
+        predicted_bg = (MODEL['constant'] +
+                        MODEL['prev_bg'] * prev_bg +
+                        MODEL['bg_slope'] * bg_slope +
+                        MODEL['di'] * di + 
+                        MODEL['di_slope'] * di_slope)
+        update.execute('''
+            update insulin_carb_smoothed_2
+            set predicted_bg = %s
+            where rtime = %s''',
+                       [predicted_bg,rtime])
+        # shift current values to prev values
+        prev_prev_bg,prev_bg,prev_di = (prev_bg,good_bg, di)
+
+
+# ================================================================
+
 if __name__ == '__main__':
     # compute_isf()
-    print((getRecentISF(0,4,40,debug=True)))
-    print((getRecentISF(0,4,50,debug=True)))
+    # print((getRecentISF(0,4,40,debug=True)))
+    # print((getRecentISF(0,4,50,debug=True)))
+    # import pdb; pdb.set_trace()
+    # long_isf_values()
+    # test_isf_at_time(min_rtime(),dateparser.parse('12/1/2014 1:00am'))
+    # isf_density()
+    # est_isf_values()
+    # test_isf_at_time()
+    # est_isf_values()
+    # est_isf_table()
+    # regression_data()
+    compute_predicted_bg()
