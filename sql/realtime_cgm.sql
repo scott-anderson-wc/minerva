94f4cdfff636f7fd9de6358d8f1f5a875db8c9f8
Scott D. Anderson <scott.anderson@wellesley.edu>
2022-09-01 14:18:16 -0400

    complete rewrite of realtime_cgm2

--- sql/realtime_cgm.sql
+++ sql/realtime_cgm.sql
@@ -2,10 +2,22 @@
 -- none of which is a date type; they are all varchar. So is mgdl,
 -- which really should be tinyint.
 
-drop table if exists realtime_cgm2;
-create table realtime_cgm2 (
+-- This was completely revised in August 2022 because of the re-written Dexcom CGM download. 
+-- We added the dexcom timestamp so that we could detect the NoData situation and to fill in
+-- past data. If there is NoData, mgdl, trend and trend_code will all be NULL
+
+-- BTW, trend_code is entirely redundant with trend. Indeed, trend == trend_code+0
+-- However, if they come up with a new code, we'll be able to store it in trend even
+-- if we can't store it in trend_code.
+
+-- I got rid of the rec_num field, since we almost never use it and it makes more sense
+-- to use (user,rtime) as the key. That also allows the ON-DUPLICATE-KEY trick. 
+
+drop table if exists realtime_cgm3;
+create table realtime_cgm3 (
     user varchar(20),
     rtime datetime,
+    dexcom_time datetime,
     mgdl smallint,
     trend tinyint,
     trend_code enum('None',     -- 0
@@ -19,24 +31,22 @@ create table realtime_cgm2 (
                'NotComputable', -- 8
                'RateOutOfRange' -- 9
                ),
-    rec_num int(10) not null auto_increment,
-    primary key (rec_num),
+    primary key (user, rtime),
     index(rtime)
 );
 
-insert into realtime_cgm2
-select user, date5f(date), mgdl,
-case
+insert into realtime_cgm3
+select user, date5f(date), NULL, min(mgdl), 
+min(case
     when trend = '' then 0
     when trend > 9 then 0
     else trend
-end,
-case
+end),
+min(case
     when trend = '' then 0
     when trend > 9 then 0
     else trend
-end,
-rec_num
+end)
 from realtime_cgm
-where mgdl <> '';
-
+where mgdl <> ''
+group by user, date5f(date);
