e9921e7d3f983022d51f41ef2c186f6dee764dcd
Scott D. Anderson <scott.anderson@wellesley.edu>
2023-11-14 17:07:34 -0500

    swapped new autoapp_to_loop_logic_inputs in cronjobs.sh

--- cronjobs.sh
+++ cronjobs.sh
@@ -42,7 +42,8 @@ $python $repo/dexcom_cgm_sample.py
 # temporary removal until migration time is fixed
 # $python $repo/autoapp_to_ics2.py cron 
 
-$python $repo/autoapp_to_loop_logic.py 
+# $python $repo/autoapp_to_loop_logic.py
+$python $repo/autoapp_to_loop_logic_inputs.py 
 $python $repo/loop_logic_testing_cgm_cron.py
 
 # temporary removal until we know this is working
