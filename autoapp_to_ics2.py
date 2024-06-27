2eebf4d87b4b745230505398de9d28dbfb6db0eb
Scott D. Anderson <scott.anderson@wellesley.edu>
2024-06-27 18:19:33 -0400

    fixes to minutes_since functions

--- autoapp_to_ics2.py
+++ autoapp_to_ics2.py
@@ -99,12 +99,18 @@ get_migration_time().
 ## ----------------------------- Migration ----------------------------------------
 
     
-def print_rows(rows):
+def print_dictionaries(rows):
     keys = rows[0].keys()
     print('\t'.join(keys))
     for row in rows:
         # can I just use values?
         row_list = [ str(row[k]) for k in keys ]
+        print('\t'.join(row_list))
+
+def print_tuples(col_names, rows):
+    print('\t'.join(col_names))
+    for row in rows:
+        row_list = [ str(val) for val in row ]
         print('\t'.join(row_list))
 
 # ================================================================
@@ -153,7 +159,7 @@ def bolus_import_s_test(conn, start_time, end_time):
                          where user_id = 7 and type = 'S' and date >= %s and date <= %s''',
                  [start_time, end_time])
     print(f'{nr} S boluses between {start_time} and {end_time}')
-    print_rows(curs.fetchall())
+    print_tuples(['date', 'type', 'value'], curs.fetchall())
     nr = bolus_import_s(conn, start_rtime, debugp=True)
     print('result: {} rows modified'.format(nr))
     nr = curs.execute(f'''select rtime, bolus_type, total_bolus_volume
@@ -161,7 +167,7 @@ def bolus_import_s_test(conn, start_time, end_time):
                          where bolus_type = 'S' and rtime >= %s and rtime < %s''',
                       [start_time, end_time])
     print(f'imported {nr} S boluses between {start_time} and {end_time}')
-    print_rows(curs.fetchall())
+    print_tuples(['rtime', 'bolus_type', 'total_bolus_volume'], curs.fetchall())
     
 def bolus_import_ds(conn, start_time, end_time, debugp=False):
     '''A DS entry is, I think, the same as an S entry, but maybe is paired
@@ -193,7 +199,7 @@ def bolus_import_ds_test(conn, start_time, end_time):
                          where user_id = 7 and type = 'DS' and date >= %s and date <= %s''',
                  [start_time, end_time])
     print(f'{nr} DS boluses between {start_time} and {end_time}')
-    print_rows(curs.fetchall())
+    print_tuples(['rtime', 'type', 'bolus_value'], curs.fetchall())
     nr = bolus_import_ds(conn, start_time, end_time, debugp=True)
     print('result: {} rows modified'.format(nr))
     nr = curs.execute(f'''select rtime, bolus_type, total_bolus_volume
@@ -201,7 +207,7 @@ def bolus_import_ds_test(conn, start_time, end_time):
                          where bolus_type = 'DS' and rtime >= %s and rtime <= %s'''
                       [start_rtime])
     print(f'{nr} DS boluses between {start_time} and {end_time}')
-    print_rows(curs.fetchall())
+    print_tuples(['rtime', 'bolus_type', 'total bolus volume'], curs.fetchall())
 
 def approx_equal(x, y, maxRelDiff = 0.0001):
     '''see https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
@@ -262,7 +268,7 @@ def extended_bolus_import_test(conn, start_time, end_time):
                            AND date >= %s and date <= %s ''',
                  [start_time, end_time])
     print(f'{nr} extended boluses between {start_time} and {end_time}')
-    print_rows(curs.fetchall())
+    print_tuples(['rtime', 'bolus type', 'bolus value', 'duration'], curs.fetchall())
     # before updating the destination TABLE, let's null out the extended_bolus_amt_12
     curs.execute(f'update {TABLE} set extended_bolus_amt_12 = null where rtime >= %s and rtime <= %s',
                  [start_time, end_time])
@@ -284,7 +290,7 @@ def extended_bolus_import_test(conn, start_time, end_time):
                          AND extended_bolus_amt_12 is not null''',
                       [start_time, end_time])
     print(f'{nr} extended boluses between {start_time} and {end_time}')
-    print_rows(curs.fetchall())
+    print_tuples(['rtime', 'bolus_type', 'extended_bolus_amt_12'], curs.fetchall())
     # let's also print the sum. Unfortunately, this sum will also include E boluses. What to do....
     # for debugging, we'll go back and set all the extended_bolus_amt_12 to null at the top of this
     curs.execute(f'select sum(extended_bolus_amt_12) from {TABLE} where rtime >= %s and rtime <= %s',
@@ -470,11 +476,14 @@ def update_minutes_since_last_meal(conn, start_time,
         # Now, work forward, incrementing the counter and resetting it to
         # zero when there are non-rescue carbs
         curs = dbi.cursor(conn)
+        logging.debug(f'iterating over all rows from {rtime} to {end_time}')
         while rtime < end_time:
-            rtime += timedelta(minutes=5)
-            curs.execute(f'''SELECT carb_code FROM {TABLE}
-                             WHERE rtime = %s''',
-                         [rtime])
+            nr = curs.execute(f'''SELECT carb_code FROM {TABLE}
+                                  WHERE rtime = %s''',
+                              [rtime])
+            if nr == 0:
+                logging.error(f'No row in {TABLE} where rtime = {rtime}')
+                return
             # notice the comma here; we're pulling the carb_code out of a tuple
             (carb_code,) = curs.fetchone()
             # print(rtime,carb_code)
@@ -486,6 +495,7 @@ def update_minutes_since_last_meal(conn, start_time,
             curs.execute(f'''UPDATE {TABLE} SET minutes_since_last_meal = %s
                              WHERE rtime = %s''',
                          [mm, rtime])
+            rtime += timedelta(minutes=5)
         if not debug:
             conn.commit()
     except Exception as err:
@@ -538,10 +548,12 @@ def update_minutes_since_last_bolus(conn,
         # zero when there is a bolus, which we define as total_bolus_volume > 0
         curs = dbi.cursor(conn)
         while rtime < end_time:
-            rtime += timedelta(minutes=5)
-            curs.execute(f'''SELECT total_bolus_volume FROM {TABLE}
-                             WHERE rtime = %s''',
-                         [rtime])
+            nr = curs.execute(f'''SELECT total_bolus_volume FROM {TABLE}
+                                  WHERE rtime = %s''',
+                              [rtime])
+            if nr == 0:
+                logging.error(f'There is no row for rtime {rtime}')
+                return
             # notice the comma here; we're pulling the carb_code out of a tuple
             (tbv,) = curs.fetchone()
             if tbv is not None and tbv > 0:
@@ -552,6 +564,7 @@ def update_minutes_since_last_bolus(conn,
             curs.execute(f'''UPDATE {TABLE} SET minutes_since_last_bolus = %s
                              WHERE rtime = %s''',
                          [mb, rtime])
+            rtime += timedelta(minutes=5)
         if not debug:
             conn.commit()
     except Exception as err:
@@ -594,72 +607,6 @@ def update_corrective_insulin(conn,
             conn.commit()
 
 # ================================================================
-
-# print('copy code for prime and refill from Milevas code')    
-
-## ----------------------------- Tests ----------------------------------------
-
-def test_conn(conn): 
-    '''Test CRUD operations in autoapp/ janice databases''' 
-    curs = conn.cursor()
-
-    # Testing read operations of autoapp tables
-    curs.execute('''select * from autoapp.basal_hour limit 5;''')
-    results = curs.fetchall()
-    print(results)
-
-    # Testing create/delete operations of janice tables
-    # curs.execute('''drop table if exists janice.mileva_test;''')
-    # curs.execute('''create table janice.mileva_test like insulin_carb_smoothed_2;''')
-
-def non_zero_duration_S_boluses(conn):
-    curs = conn.cursor()
-    curs.execute('''select bolus_id, date, type, value, duration 
-                    from autoapp.bolus
-                    where type = 'S' and duration > 5''')
-    data = curs.fetchall()
-    print('{} non-zero duration S events'.format(len(data)))
-    for row in data:
-        print((5*'{}\t').format(*row))
-
-def zero_duration_E_boluses(conn):
-    curs = conn.cursor()
-    curs.execute('''select bolus_id, date, type, value, duration 
-                    from autoapp.bolus
-                    where type = 'E' and duration = 0''')
-    data = curs.fetchall()
-    print('{} zero-duration E events'.format(len(data)))
-    for row in data:
-        print((5*'{}\t').format(*row))
-
-def simultaneous_bolus_and_basal(conn):
-    curs = conn.cursor()
-    curs.execute('''select *
-                    from autoapp.bolus inner join autoapp.basal_hour 
-                    on (bolus.date = basal_hour.date)''')
-    data = curs.fetchall()
-    print('{} simultaneous bolus and basal_hour events'.format(len(data)))
-    for row in data:
-        print((5*'{}\t').format(*row))
-
-# ================================================================
-
-def migrate_cgm(conn=None):
-    '''This migrates any cgm values that we don't already have in ICS2. '''
-    if conn is None:
-        conn = dbi.connect()
-    curs = dbi.cursor(conn)
-    curs.execute('select max(rtime) from insulin_carb_smoothed_2 where cgm is not null');
-    start_cgm = curs.fetchone()[0]
-    logging.debug('migrating cgm data starting at {}'.format(start_cgm))
-    curs.execute('''UPDATE insulin_carb_smoothed_2 AS ics 
-                        INNER JOIN realtime_cgm2 AS rt USING (rtime)
-                    SET ics.cgm = rt.mgdl
-                    WHERE rtime >= %s''',
-                 [start_cgm])
-    conn.commit()
-                    
-# ================================================================
 # Dynamic Insulin (D)I and Dynamic Carbs (DC)
 # Note that an older batch-mode computation using generators was
 # done in dynamic_insulin.py
@@ -670,7 +617,6 @@ def migrate_cgm(conn=None):
 IAC_filename = 'reverse_engineered_iac_2022-12-20.csv'
 
 IAC = None
-CAC = None
 
 def normalize_curve(float_list):
     total = sum(float_list)
@@ -762,6 +708,229 @@ def update_dynamic_insulin(conn,
         if commit:
             conn.commit()
 
+# ================================================================
+# Dynamic Carbs
+
+# We currently use the same curve for breakfast, lunch, and snack, but
+# we allow for different curves. The CAC variable is a dictionary of
+# four curves, indexed by meal name: breakfast, lunch, snack and
+# dinner.
+
+CAC_filename = 'carb_curves.csv'
+CAC = None
+
+def read_carb_action_curves(test=False):
+    '''Return the carb absorption curves as a dictionary of arrays,
+projecting three columns of the spreadsheet (as a CSV file). The bls
+column is replicated as breakfast, lunch, and snack, so that that we
+can look up the curve using the carb_code.  We'll also replicate it
+for before6 and after9, since those happen every so often.
+
+    '''
+    global CAC
+    if test:
+        CAC = {
+            'rescue': [ 0, 0.5, 1.0, 0.75, 0.5, 0.25, 0 ],
+            # symmetrical, short
+            'bls': [ 0, 0.2, 0.5, 0.8, 1.0, 0.8, 0.5, 0.2, 0 ],
+            # skewed, long
+            'dinner': [ 0, 0.2, 0.5, 0.8, 1.0, 0.9, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0 ]
+            }
+    else:
+        if CAC is not None:
+            logging.debug(f'Using cached CAC')
+            return CAC
+        rescue_vals = []
+        bls_vals = []
+        dinner_vals = []
+        with open(CAC_filename, 'rU') as csvfile:
+            reader = csv.reader(csvfile) # default format is Excel
+            # skip headers
+            headers = next(reader)
+            for row in reader:
+                if row[1] != '':
+                    rescue_vals.append(float(row[1]))
+                if row[2] != '':
+                    bls_vals.append(float(row[2]))
+                if row[3] != '':
+                    dinner_vals.append(float(row[3]))
+        # after the loop, and 
+        CAC = {'rescue': normalize_curve(rescue_vals),
+               'bls': normalize_curve(bls_vals),
+               'dinner': normalize_curve(dinner_vals)}
+    # after the if/else, replicate the bls values
+    CAC['breakfast'] = CAC['bls']
+    CAC['lunch'] = CAC['bls']
+    CAC['snack'] = CAC['bls']
+    CAC['after9'] = CAC['bls']
+    CAC['before6'] = CAC['bls']
+    return CAC
+
+def dc_worker(window, index, cac):
+    '''Computes a returns a value of DC with a sliding window where
+    the first index into the window is 'index' and the current CAC
+    curve is as given, indexed from zero.
+
+    '''
+    if len(window) != len(cac):
+        raise ValueError('window and cac must be lists of the same length')
+    win_width = len(cac)
+    sum = 0 
+    for i in range(win_width):
+        # remember, iterate through CAC in reverse order
+        weight = cac[i]
+        j = (index - i) % win_width
+        carbs = window[j]
+        sum += weight * carbs
+    return sum
+
+def update_dynamic_carbs(conn,
+                         start_time,
+                         end_time=date_ui.to_rtime(datetime.now()),
+                         commit=True):
+    '''Compute DC values from start_time to end_time, changing the CAC
+    as needed with each meal. Otherwise, similar to
+    update_dynamic_insulin.'''
+    start_time = date_ui.to_rtime(start_time)
+    end_time = date_ui.to_rtime(end_time)
+
+    logging.info(f'update_dynamic_carbs from {start_time} to {end_time}')
+    cac_curves = read_carb_action_curves()
+    curs = dbi.cursor(conn)
+    longest_curve_len = max([len(cac) for cac in cac_curves.values() ])
+    logging.debug(f'The longest cac curve is {longest_curve_len}')
+    meal_lookback = timedelta(minutes=5*longest_curve_len)
+    logging.debug(f'The meal lookback is {meal_lookback}')
+    curs.execute(f'''SELECT rtime, carbs, carb_code FROM {TABLE}
+                     WHERE rtime >= %s and rtime < %s
+                     AND carb_code is not NULL''',
+                 [start_time - meal_lookback, start_time])
+    recent_meals = curs.fetchall()
+    curr_rtime = start_time
+
+    # Okay, here's the main loop over all rtimes from start to end
+    while curr_rtime <= end_time:
+        curs.execute(f'''SELECT rtime, carbs, carb_code FROM {TABLE}
+                         WHERE rtime = %s''',
+                     [curr_rtime])
+        curr_row = curs.fetchone()
+        if curr_row[2] is not None:
+            # a new meal, so add it to the list
+            logging.debug(f'new meal {curr_row[2]} at time {curr_rtime}')
+            recent_meals.append(curr_row)
+        # Now, compute DC. We use an index loop so we can pop(i) to
+        # remove a meal that is too old
+        curr_dc = 0
+        for i in range(len(recent_meals)):
+            meal_rtime, meal_carbs, meal_carb_code = recent_meals[i]
+            meal_cac = cac_curves[meal_carb_code]
+            # meal_age is in 5-minute time steps, so a meal 30 minutes
+            # ago has an age of 6
+            meal_age = int((start_time - meal_rtime)/60*5)
+            if meal_age > len(meal_cac):
+                recent_meals.pop(i) # remove this meal
+            else:
+                curr_dc += meal_carbs * meal_cac[meal_age]
+        # Done computing DC
+        logging.debug(f'At {curr_rtime} DC is {curr_dc}')
+        curs.execute(f'UPDATE {TABLE} SET dynamic_carbs = %s WHERE rtime = %s',
+                     [curr_dc, curr_rtime])
+        if commit:
+            conn.commit()
+        curr_rtime += timedelta(minutes=5)
+    # After the loop
+    logging.info('done update_dynamic_carbs from {start_time} to {end_time}')
+
+def update_dynamic_carbs_test(conn=None):
+    '''Use a test table to test this, since it necessarily needs to
+    read and write database tables; trying to do it with arrays is, I
+    think, too weird.'''
+    curs = dbi.cursor(conn)
+    global TABLE
+    TABLE = 'janice.mileva_test'
+    curs.execute(f'''drop table if exists {TABLE};''')
+    curs.execute(f'''create table {TABLE} like janice.insulin_carb_smoothed_2;''')
+    conn.commit()
+    date1, date2 = '2024-06-01 16:00', '2024-06-03'
+    print('fill forward')
+    fill_forward_between(conn, date1, date2)
+    print('insert test data')
+    for row in [
+            ['2024-06-01 18:00', 40, 'dinner'],
+            ['2024-06-02  7:00', 10, 'breakfast'],
+            # scenario of overlapping meals
+            ['2024-06-02 14:50', 20, 'lunch'],
+            ['2024-06-02 16:00', 10, 'snack'],
+            ['2024-06-02 17:00', 40, 'dinner']
+            ]:
+        rtime, carbs, carb_code = row
+        rtime = date_ui.to_rtime(rtime)
+        curs.execute(f'UPDATE {TABLE} SET carbs = %s, carb_code = %s WHERE rtime = %s',
+                     [carbs, carb_code, rtime])
+    conn.commit()
+    print('before')
+    curs.execute(f'SELECT rtime, carbs, carb_code, dynamic_carbs as dc FROM {TABLE}')
+    print_tuples(['rtime', 'carbs', 'carb_code', 'dynamic_carbs'], curs.fetchall())
+
+    # Okay, that's the test data. Let's do it:
+    update_dynamic_carbs(conn, date1, date2)
+
+    print('after')
+    curs.execute(f'SELECT rtime, carbs, carb_code, dynamic_carbs as dc FROM {TABLE}')
+    print_tuples(['rtime', 'carbs', 'carb_code', 'dynamic_carbs'], curs.fetchall())
+
+# ================================================================
+# tests and inqueries
+
+# print('copy code for prime and refill from Milevas code')    
+
+def non_zero_duration_S_boluses(conn):
+    curs = conn.cursor()
+    curs.execute('''select bolus_id, date, type, value, duration 
+                    from autoapp.bolus
+                    where type = 'S' and duration > 5''')
+    data = curs.fetchall()
+    print('{} non-zero duration S events'.format(len(data)))
+    for row in data:
+        print((5*'{}\t').format(*row))
+
+def zero_duration_E_boluses(conn):
+    curs = conn.cursor()
+    curs.execute('''select bolus_id, date, type, value, duration 
+                    from autoapp.bolus
+                    where type = 'E' and duration = 0''')
+    data = curs.fetchall()
+    print('{} zero-duration E events'.format(len(data)))
+    for row in data:
+        print((5*'{}\t').format(*row))
+
+def simultaneous_bolus_and_basal(conn):
+    curs = conn.cursor()
+    curs.execute('''select *
+                    from autoapp.bolus inner join autoapp.basal_hour 
+                    on (bolus.date = basal_hour.date)''')
+    data = curs.fetchall()
+    print('{} simultaneous bolus and basal_hour events'.format(len(data)))
+    for row in data:
+        print((5*'{}\t').format(*row))
+
+# ================================================================
+
+def migrate_cgm(conn=None):
+    '''This migrates any cgm values that we don't already have in ICS2. '''
+    if conn is None:
+        conn = dbi.connect()
+    curs = dbi.cursor(conn)
+    curs.execute('select max(rtime) from insulin_carb_smoothed_2 where cgm is not null');
+    start_cgm = curs.fetchone()[0]
+    logging.debug('migrating cgm data starting at {}'.format(start_cgm))
+    curs.execute('''UPDATE insulin_carb_smoothed_2 AS ics 
+                        INNER JOIN realtime_cgm2 AS rt USING (rtime)
+                    SET ics.cgm = rt.mgdl
+                    WHERE rtime >= %s''',
+                 [start_cgm])
+    conn.commit()
+                    
 # ================================================================
 # migrate data since previous update
 
